edebf68882a04c96727adda7dbe221e203c94667
shakilsurani <shakil.surani@gmail.com>
2016-08-18 10:58:44 +1000

    Removed unwanted dll's

--- MovieService/App_Start/Startup.Auth.cs
+++ MovieService/App_Start/Startup.Auth.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
 using MovieService.Models;
@@ -28,7 +26,7 @@ namespace MovieService
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            //app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Configure the application for OAuth based flow
             //PublicClientId = "self";
